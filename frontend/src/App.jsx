90c6bd4c65b09586e1a6005f772643bbb2e3a34b
Yashaswini Karanam <kyashaswini221106@gmail.com>
2026-08-02 00:04:19 +0530

    Update login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,11 +9,17 @@ import Login from './pages/Login'
 import PlaceOrder from './pages/PlaceOrder'
 import Orders from './pages/Orders'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import SearchBar from './components/SearchBar'
+import {ToastContainer} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 const App = () => {
   return (
     <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+      <ToastContainer />
       <Navbar />
+      <SearchBar />
 
     <Routes>
     <Route path='/' element={<Home />} />
@@ -26,6 +32,8 @@ const App = () => {
     <Route path='/place-order' element={<PlaceOrder />} />
     <Route path='/orders' element={<Orders />} />
     </Routes>
+
+    <Footer />
     </div>
   )
 }
