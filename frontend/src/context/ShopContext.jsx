0ea34e98d4705e18eb41be27a8a94a0ba43e6360
Yashaswini Karanam <kyashaswini221106@gmail.com>
2026-08-02 00:04:04 +0530

    Update frontend skeleton

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,5 +1,7 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { products } from "../assets/assets";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 // eslint-disable-next-line react-refresh/only-export-components
 export const ShopContext = createContext();
@@ -9,8 +11,82 @@ const ShopContextProvider = (props)=> {
     const currency='$'
     const deliveryfee=10
 
+    const [search,setSearch] = useState('')
+    const [showSearch,setShowSearch] = useState(true)
+    const [cartItems,setCartItems] = useState({})
+
+    const navigate = useNavigate()
+
+    const addToCart = async(itemId,size)=> {
+        let cartData = structuredClone(cartItems)
+
+        if(!size) {
+            toast.error('Select Product Size')
+            return;
+        }
+
+        if(cartData[itemId]) {
+            if(cartData[itemId][size]) {
+                cartData[itemId][size]+=1
+            } else {
+                cartData[itemId][size]=1
+            }
+        } else {
+            cartData[itemId] ={}
+            cartData[itemId][size]=1
+        }
+        setCartItems(cartData)
+    }
+
+    const getCartCount = ()=> {
+        let totalcount =0
+        for(const items in cartItems) {
+            for(const item in cartItems[items]) {
+                try {
+                    if(cartItems[items][item]>0) {
+                        totalcount+=cartItems[items][item]
+                    }
+                } catch(error) {
+                    toast.error(error.message)
+                }
+            }
+        }
+        return totalcount
+    }
+
+    const updateQuantity  = async(itemId,size,quantity)=> {
+        let cartData = structuredClone(cartItems)
+
+        cartData[itemId][size] = quantity
+
+        setCartItems(cartData)
+    }
+
+    const getCartAmount = () => {
+        let totalAmount = 0
+        for(const items in cartItems) {
+                let iteminfo = products.find((product)=>product._id=== items)
+                for(const item in cartItems[items]) {
+                    try {
+                        if(cartItems[items][item]>0) {
+                            totalAmount+=iteminfo.price * cartItems[items][item]
+                        }
+                    } catch(error) {
+                        toast.error(error.message)
+                    }
+                }
+        }
+        return totalAmount
+    }
+
+
     const value = {
-        products,currency,deliveryfee
+        products,currency,deliveryfee,
+        search,setSearch,showSearch,setShowSearch,
+        cartItems,addToCart,
+        getCartCount,updateQuantity,
+        getCartAmount,
+        navigate
     }
 
     return (
