6fe3d4bd14d4209f0b1c18cb1a2863df2723aee0
Yashaswini Karanam <kyashaswini221106@gmail.com>
2026-08-02 23:30:55 +0530

    Create user,product routes

---
+++ backend/routes/productRouter.js
@@ -0,0 +1,14 @@
+import express from 'express'
+
+import { listProducts,addProduct,removeProduct,singleProduct } from '../controllers/productController.js'
+import upload from '../middlewares/multer.js'
+import adminAuth from '../middlewares/adminAuth.js'
+
+const productRouter= express.Router()
+
+productRouter.post('/add',adminAuth,upload.fields([{name:'image1',maxCount:1},{name:'image2',maxCount:1},{name:'image3',maxCount:1},{name:'image4',maxCount:1}]),addProduct)
+productRouter.post('/remove',adminAuth,removeProduct)
+productRouter.post('/single',singleProduct)
+productRouter.get('/list',listProducts)
+
+export default productRouter
