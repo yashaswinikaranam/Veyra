e7469834b8703920364cddaa033efe6517d15087
Yashaswini Karanam <kyashaswini221106@gmail.com>
2026-08-02 23:30:00 +0530

    Add middleware to backend api requests

---
+++ backend/middlewares/adminAuth.js
@@ -0,0 +1,20 @@
+import jwt from 'jsonwebtoken'
+
+const adminAuth = async(req,res,next)=> {
+    try {
+        const {token} = req.headers
+        if(!token) {
+           return res.json({success:false,message:"Not Authorized!"})
+        }
+
+        const token_decode = jwt.verify(token,process.env.JWT_SECRET)
+        if(token_decode!==process.env.ADMIN_EMAIL+process.env.ADMIN_PASSWORD) {
+            return res.json({success:false,message:"Not Authorized!"})
+        }
+        next()
+    } catch(error) {
+        res.json({success:false,message:error.message})
+    }
+}
+
+export default adminAuth
